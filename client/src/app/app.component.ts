80cbdd673891aa6e6ac0ac89f88351b05547878f
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2024-12-21 17:37:12 +0530

    removed unnecessary code

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
-import { NgxSpinner, NgxSpinnerComponent } from 'ngx-spinner';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent]
+  imports: [RouterOutlet, NavComponent, NgxSpinnerComponent]
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
