421cb88c94bdf9b1c70d15bbaf1c5220e630d011
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2025-01-05 12:12:55 +0530

    Implemented Unit of Work and fixed bugs

--- API/Interfaces/ILikesRepository.cs
+++ API/Interfaces/ILikesRepository.cs
@@ -9,9 +9,6 @@ public interface ILikesRepository
     Task<UserLike?> GetUserLike(int sourceUserId, int targetUserId);
     Task<PagedList<MemberDto>> GetUserLikes(LikesParams likesParams);
     Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId);
-
     void DeleteLike(UserLike like);
     void AddLike(UserLike like);
-
-    Task<bool> SaveChanges();
 }
